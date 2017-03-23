ee22db39a30355316282d78077b472c008c7cd5b
Jan Griesfeller <jgriesfeller@users.noreply.github.com>
2017-03-23 13:43:48 +0000

    removed a lot of clutter from the routine

--- python3/ReadNetcdf/ReadNetcdf.py
+++ python3/ReadNetcdf/ReadNetcdf.py
@@ -3,15 +3,8 @@
 ################################################################
 
 import pdb
-import argparse
-import sys
-sys.path.append('/lustre/storeB/project/aerocom/lib/aerocom-data-correction-tools/')
 import os
 from netCDF4 import Dataset
-import numpy as np
-import pandas as pd
-import xarray as xr
-
 
 def ReadNetcdf(InFile, c_VarName=None, VerboseFlag=False, DebugFlag=False):
 	# Routine to read a netcdf file entirely or just a list of variables into a nested dictionary.
@@ -69,34 +62,4 @@ def ReadNetcdf(InFile, c_VarName=None, VerboseFlag=False, DebugFlag=False):
 	return NetcdfData, Dims
 
 
-if __name__ == '__main__':
-
-	dict_Param={}
-	parser = argparse.ArgumentParser(description='CalcPres: Calculate proper pressure coordinate\n\n')
-	#epilog='RI#eturns some general statistics')
-	parser.add_argument("infile", help="infile")
-	parser.add_argument("outfile", help="outfile")
-	parser.add_argument("-v","--var", help="Variable")
-	#parser.add_argument("-a","--allusers", help="analyse all logfiles, not just your own", action='store_true')
-	#parser.add_argument("-l", help="")
-
-	args = parser.parse_args()
-
-	if args.infile:
-		dict_Param['infile']=args.infile
-	else:
-		dict_Param['infile']=False
-
-	if args.outfile:
-		dict_Param['outfile']=args.outfile
-	else:
-		dict_Param['outfile']=False
-
-	if args.var:
-		dict_Param['var']=args.var
-	else:
-		dict_Param['var']=False
-
-	InData, Dims=ReadNetcdf(dict_Param['infile'], c_VarName=None, VerboseFlag=False, DebugFlag=False)
-	pdb.set_trace()
 
